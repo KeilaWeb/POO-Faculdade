8aa2f9d29dbc86266f8cd8466e3c557efb163129
Keila D. Barreto <91154547+KeilaWeb@users.noreply.github.com>
2023-09-21 13:53:51 -0300

    Novoo projeto
    Criado Gestor de Pessoas

---
+++ src/cursos/Faculdade/setembro/gestorDePessoas/GestorPessoas.java
@@ -0,0 +1,36 @@
+class GestorPessoas {
+    private List<Pessoa> pessoas = new ArrayList<>();
+
+    public void addPessoa(Pessoa pessoa) {
+        pessoas.add(pessoa);
+    }
+
+    public void removePessoa(int indice) {
+        if (indice >= 0 && indice < pessoas.size()) {
+            pessoas.remove(indice);
+        } else {
+            System.out.println("Índice inválido.");
+        }
+    }
+
+    public Pessoa buscarPessoa(String nome) {
+        for (Pessoa pessoa : pessoas) {
+            if (pessoa.getNome().equalsIgnoreCase(nome)) {
+                return pessoa;
+            }
+        }
+        return null; // Pessoa não encontrada
+    }
+
+    public void limparPessoas() {
+        pessoas.clear();
+    }
+
+    public void ordenarPessoas() {
+        Collections.sort(pessoas, Comparator.comparing(Pessoa::getNome));
+    }
+
+    public List<Pessoa> getPessoas() {
+        return pessoas;
+    }
+}
